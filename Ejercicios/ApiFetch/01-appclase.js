c60fcbb92e21855951a7b35b71cdf3e5474cbcc3
Rubirojo05 <rubenrojomoreno@gmail.com>
2025-02-17 08:26:03 +0100

    17/02

--- Ejercicios/ApiFetch/01-appclase.js
+++ Ejercicios/ApiFetch/01-appclase.js
@@ -27,8 +27,40 @@ function obtenerDatosJson() {
         })
         .then(datos => {
             console.log(datos);
+            mostrarHTML(datos);
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
+
+const cargarJsonArrayBtn = document.querySelector('#cargarJSONarray');
+cargarJsonArrayBtn.addEventListener('click', obtenerDatosJsonArray );
+
+function obtenerDatosJsonArray() {
+    const url = 'empleados.json';
+    fetch(url)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(datos => {
+            datos.forEach(empleado => mostrarHTML(empleado));
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function mostrarHTML ({empresa, id, nombre, trabajo}){
+    const contenido = document.querySelector('#contenido');
+    
+    const empleados = `
+    <p>Empleado: ${nombre}</p>
+    <p>ID: ${id}</p>
+    <p>Empresa: ${empresa}</p>
+    <p>Trabajo: ${trabajo}</p>
+    `;
+
+    contenido.innerHTML += empleados;
+}
+
